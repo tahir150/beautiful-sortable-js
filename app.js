546a01ff577d405d8210c4e053f03614225e3d96
tahir150 <tahirtv.islam@gmail.com>
2022-11-14 16:17:34 +0500

    improved UI

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ divsToSort.forEach((item, i) => {
   div.onclick = () => alert("fallback orignal");
 
   const sortable = new Sortable(item, {
-    // containment: parent,
-    // itemClass: "tahir",
-    // draggingClass : "tahir",
-    // zoom: 0.5,
-    // disabledClass : "",
-    containers: "sort-container",
-      fallBackElement: item.classList.contains("fallback")
-        ? `<div class="fallback-element">
-      <span>F - Hello I am a Element 10</span>
-    </div>`
-        : null,
-    // fallBackClone: false,
-    onSort: (details) => {
-      console.log(details);
-    },
+    // itemClass: "item-class", // class applies to every sortable element
+    // draggingClass: "dargging", // class will apply when dragging start
+    // zoom: 1, // it is viewport zoom value (if have css zoom property)
+    // disabledClass: "disabled", // class will apply if sorting is disable
+    // containers: "sort-container", // comma seperated appendable boxes classes
+    fallBackElement: item.classList.contains("fallback") // it is not sortable, it just append this fallback html
+      ? `<div class="fallback-element">
+         <span>I am fallback</span>
+         </div>`
+      : null,
+    // fallBackClone: false, // if will drop a clone of fallback element
+    // onStart: (startDetail) => {
+    //   // it will trigger when you start sorting
+    //   console.log(startDetail);
+    // },
+    // onDrop: (details) => {
+    //   // it will trigger after dropped even it is sorted or not
+    //   console.log(details);
+    // },
+    // onSort: (details) => {
+    //   // if element has change or sorted it takes gurantee to trigger after sorting
+    //   console.log(details);
+    // },
   });
-    // sortable.disable();
+  // sortable.disable();
 });
